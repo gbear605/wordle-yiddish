9fcc2b39e2fd482b2399149b78e69773dfac3b98
haithamleo <haitham.khamiasi@rihal.om>
2022-01-16 15:11:27 +0400

    enhancement: apply prettier

--- src/lib/statuses.ts
+++ src/lib/statuses.ts
@@ -1,109 +1,109 @@
-import { solution } from "./words";
+import { solution } from './words'
 
-export type CharStatus = "absent" | "present" | "correct";
+export type CharStatus = 'absent' | 'present' | 'correct'
 
 export type CharValue =
-  | "א"
-  | "אַ"
-  | "אָ"
-  | "ב"
-  | "בּ"
-  | "בֿ"
-  | "ג"
-  | "ד"
-  | "ה"
-  | "ו"
-  | "ז"
-  | "ח"
-  | "ט"
-  | "י"
-  | "כ"
-  | "כּ"
-  | "ל"
-  | "מ"
-  | "נ"
-  | "ס"
-  | "ע"
-  | "פּ"
-  | "פֿ"
-  | "צ"
-  | "ק"
-  | "ר"
-  | "ש"
-  | "ת"
-  | "תּ"
-  | "װ"
-  | "ױ"
-  | "ײ"
-  | "ײַ";
+  | 'א'
+  | 'אַ'
+  | 'אָ'
+  | 'ב'
+  | 'בּ'
+  | 'בֿ'
+  | 'ג'
+  | 'ד'
+  | 'ה'
+  | 'ו'
+  | 'ז'
+  | 'ח'
+  | 'ט'
+  | 'י'
+  | 'כ'
+  | 'כּ'
+  | 'ל'
+  | 'מ'
+  | 'נ'
+  | 'ס'
+  | 'ע'
+  | 'פּ'
+  | 'פֿ'
+  | 'צ'
+  | 'ק'
+  | 'ר'
+  | 'ש'
+  | 'ת'
+  | 'תּ'
+  | 'װ'
+  | 'ױ'
+  | 'ײ'
+  | 'ײַ'
 
 export const getStatuses = (
   guesses: string[]
 ): { [key: string]: CharStatus } => {
-  const charObj: { [key: string]: CharStatus } = {};
+  const charObj: { [key: string]: CharStatus } = {}
 
   guesses.forEach((word) => {
-    word.split("").forEach((letter, i) => {
+    word.split('').forEach((letter, i) => {
       if (!solution.includes(letter)) {
         // make status absent
-        return (charObj[letter] = "absent");
+        return (charObj[letter] = 'absent')
       }
 
       if (letter === solution[i]) {
         //make status correct
-        return (charObj[letter] = "correct");
+        return (charObj[letter] = 'correct')
       }
 
-      if (charObj[letter] !== "correct") {
+      if (charObj[letter] !== 'correct') {
         //make status present
-        return (charObj[letter] = "present");
+        return (charObj[letter] = 'present')
       }
-    });
-  });
+    })
+  })
 
-  return charObj;
-};
+  return charObj
+}
 
 export const getGuessStatuses = (guess: string): CharStatus[] => {
-  const splitSolution = solution.split("");
-  const splitGuess = guess.split("");
+  const splitSolution = solution.split('')
+  const splitGuess = guess.split('')
 
-  const solutionCharsTaken = splitSolution.map((_) => false);
+  const solutionCharsTaken = splitSolution.map((_) => false)
 
-  const statuses: CharStatus[] = Array.from(Array(guess.length));
+  const statuses: CharStatus[] = Array.from(Array(guess.length))
 
   // handle all correct cases first
   splitGuess.forEach((letter, i) => {
     if (letter === splitSolution[i]) {
-      statuses[i] = "correct";
-      solutionCharsTaken[i] = true;
-      return;
+      statuses[i] = 'correct'
+      solutionCharsTaken[i] = true
+      return
     }
-  });
+  })
 
   splitGuess.forEach((letter, i) => {
-    if (statuses[i]) return;
+    if (statuses[i]) return
 
     if (!splitSolution.includes(letter)) {
       // handles the absent case
-      statuses[i] = "absent";
-      return;
+      statuses[i] = 'absent'
+      return
     }
 
     // now we are left with "present"s
     const indexOfPresentChar = splitSolution.findIndex(
       (x, index) => x === letter && !solutionCharsTaken[index]
-    );
+    )
 
     if (indexOfPresentChar > -1) {
-      statuses[i] = "present";
-      solutionCharsTaken[indexOfPresentChar] = true;
-      return;
+      statuses[i] = 'present'
+      solutionCharsTaken[indexOfPresentChar] = true
+      return
     } else {
-      statuses[i] = "absent";
-      return;
+      statuses[i] = 'absent'
+      return
     }
-  });
+  })
 
-  return statuses;
-};
+  return statuses
+}
