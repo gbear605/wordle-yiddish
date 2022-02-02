a27a79cbc919462da5993bf3b2ee04c8eb0a5b10
gbear605 <developer@vivissi.com>
2022-02-01 21:53:30 -0500

    Fix order of info words

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -20,33 +20,33 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="ל" status="correct" />
-        <Cell value="ר" />
-        <Cell value="י" />
-        <Cell value="נ" />
         <Cell value="ש" />
+        <Cell value="נ" />
+        <Cell value="י" />
+        <Cell value="ר" />
+        <Cell value="ל" status="correct" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         דער אות ל איז אין דעם וואָרט און אין דעם ריכטיקן אָרט.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="ל" />
-        <Cell value="ט" />
-        <Cell value="נ" status="present" />
-        <Cell value="י" />
         <Cell value="ב" />
+        <Cell value="י" />
+        <Cell value="נ" status="present" />
+        <Cell value="ט" />
+        <Cell value="ל" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         דער אות נ איז אין דעם וואָרט אָבער נישט אין דעם ריכטיקן אָרט.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="ע" />
-        <Cell value="ק" />
         <Cell value="אַ" />
         <Cell value="ט" status="absent" />
         <Cell value="אַ" />
+        <Cell value="ק" />
+        <Cell value="ע" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         דער אות ט איז נישט אין דעם וואָרט.
